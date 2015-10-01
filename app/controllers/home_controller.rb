ba4f5026674826bac40c77e6e2ad6ff43435edb7
kimzusong <challengekzs@gmail.com>
2015-10-01 07:23:49 +0000

    RailsBegin

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+    
+    def history
+    end
+    
+    def vision
+    end
+    
+    def interests
+    end
+    
+end
